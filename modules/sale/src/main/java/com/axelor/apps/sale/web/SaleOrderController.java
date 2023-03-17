0e489d58f88b083a07c8242bd8b31ee3e872bd9b
Maxence GALLANCHER <m.gallancher@axelor.com>
2023-03-17 15:47:13 +0100

    Add dummyFields example

--- modules/sale/src/main/java/com/axelor/apps/sale/web/SaleOrderController.java
+++ modules/sale/src/main/java/com/axelor/apps/sale/web/SaleOrderController.java
@@ -18,14 +18,21 @@
 package com.axelor.apps.sale.web;
 
 import com.axelor.apps.sale.service.saleorder.SaleOrderInvoicingService;
+import com.axelor.base.db.Partner;
+import com.axelor.db.JPA;
+import com.axelor.db.Model;
 import com.axelor.inject.Beans;
 import com.axelor.meta.schema.actions.ActionView;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
 import com.axelor.sale.db.Invoice;
+import com.axelor.sale.db.Product;
 import com.axelor.sale.db.SaleOrder;
 import com.axelor.sale.db.repo.SaleOrderRepository;
+import java.math.BigDecimal;
+import java.util.Map;
+import javax.validation.constraints.NotNull;
 
 public class SaleOrderController {
 
@@ -35,7 +42,9 @@ public class SaleOrderController {
 
     SaleOrder ctxSaleOrder = context.asType(SaleOrder.class);
 
-    SaleOrder managedSaleOrder = Beans.get(SaleOrderRepository.class).find(ctxSaleOrder.getId());
+    Long id = ctxSaleOrder.getId();
+
+    SaleOrder managedSaleOrder = Beans.get(SaleOrderRepository.class).find(id);
 
     Invoice invoice = Beans.get(SaleOrderInvoicingService.class).processInvoicing(managedSaleOrder);
 
@@ -47,5 +56,53 @@ public class SaleOrderController {
             .add("form", "invoice-form")
             .context("_showRecord", invoice.getId())
             .map());
+  }
+
+  public void showDummyValues(ActionRequest request, ActionResponse response) {
+
+    Context context = request.getContext();
+    Object ctxPartner = context.get("_xPartner");
+    if (ctxPartner == null) {
+      response.setError("Empty partner");
+      return;
+    }
+    Partner partner = findObject(Partner.class, ctxPartner);
+    Object ctxQty = context.get("_xQty");
+    if (ctxQty == null) {
+      response.setError("Empty qty");
+      return;
+    }
+    BigDecimal qty = new BigDecimal((String) ctxQty);
+
+    Object ctxProduct = context.get("_xProduct");
+    if (ctxProduct == null) {
+      response.setError("Empty product");
+      return;
+    }
+    Product product = findObject(Product.class, ctxProduct);
+    // In details:
+    //	Object object = context.get("_xProduct");
+    ////	 {
+    ////		 id -> 41
+    ////		 name -> Produit test
+    ////		 unitPrice -> 44.00
+    ////	 }
+    //
+    //	Map<String, Object> produitSousFormeDeMap = (Map<String, Object>) object;
+    //
+    //	Object idNonFormatté = produitSousFormeDeMap.get("id");
+    //
+    //	long id = Long.parseLong(idNonFormatté.toString());
+    //
+    //    Product product = Beans.get(ProductRepository.class).find(id);
+
+    response.setAlert("" + product + "<br/><br/>" + partner + "<br/><br/>" + qty);
+  }
+
+  public static <T extends Model> T findObject(@NotNull Class<T> tClass, @NotNull Object object) {
+    if (((Map<String, Object>) object).get("id") == null) {
+      return null;
+    }
+    return JPA.find(tClass, Long.parseLong(((Map<String, Object>) object).get("id").toString()));
   }
 }
